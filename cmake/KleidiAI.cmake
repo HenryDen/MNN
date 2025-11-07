d675cf4105f17146a4290fce2218a68bda721ecf
Shuheng Deng <shuheng.deng@arm.com>
2025-11-07 16:21:37 +0800

    Integrate fp32 depth-wise kernels into MNN
    Change-Id: I9ce5072914a1c272beabc3fcce9e928afb48a44e

--- cmake/KleidiAI.cmake
+++ cmake/KleidiAI.cmake
@@ -90,7 +90,9 @@ function (download_kleidiai_and_collect_sources)
         ${KLEIDIAI_SRC_DIR}/kai/ukernels/matmul/matmul_clamp_f16_f16p_f16p/
         ${KLEIDIAI_SRC_DIR}/kai/ukernels/matmul/matmul_clamp_f16_f16_f16p/
         ${KLEIDIAI_SRC_DIR}/kai/ukernels/matmul/imatmul_clamp_f32_f32p_f32p/
-        ${KLEIDIAI_SRC_DIR}/kai/ukernels/matmul/imatmul_clamp_f16_f16p_f16p/)
+        ${KLEIDIAI_SRC_DIR}/kai/ukernels/matmul/imatmul_clamp_f16_f16p_f16p/
+        ${KLEIDIAI_SRC_DIR}/kai/ukernels/dwconv/pack/
+        ${KLEIDIAI_SRC_DIR}/kai/ukernels/dwconv/dwconv_f32_f32_f32p/)
 
     file(GLOB kleidiai_pack_sources
         "${KLEIDIAI_SRC_DIR}/kai/ukernels/matmul/pack/kai_lhs_quant_pack_qsi8d32pscalef32_f16_neon.c"
@@ -195,6 +197,17 @@ function (download_kleidiai_and_collect_sources)
         "${KLEIDIAI_SRC_DIR}/kai/ukernels/matmul/matmul_clamp_f32_qsi8d32p_qai4c32p/*dot_asm.S"
     )
     list(APPEND KLEIDIAI_FILES_SME2 ${matmul_clamp_f32_qsi8d32p_qai4c32p_sme2_sources})
+
+    file(GLOB dwconv_pack_sources
+        "${KLEIDIAI_SRC_DIR}/kai/ukernels/dwconv/pack/*.c"
+    )
+    list(APPEND KLEIDIAI_FILES_SME2 ${dwconv_pack_sources})
+
+    file(GLOB dwconv_f32_f32_f32p_sme2_sources
+        "${KLEIDIAI_SRC_DIR}/kai/ukernels/dwconv/dwconv_f32_f32_f32p/*.c"
+        "${KLEIDIAI_SRC_DIR}/kai/ukernels/dwconv/dwconv_f32_f32_f32p/*.S"
+    )
+    list(APPEND KLEIDIAI_FILES_SME2 ${dwconv_f32_f32_f32p_sme2_sources})
 
     set_source_files_properties(
         ${MNN_SOURCES_KLEIDIAI}
